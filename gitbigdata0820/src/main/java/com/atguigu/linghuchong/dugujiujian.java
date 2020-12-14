213a90ec39df10c1f81f77df9726419a54f5a6d0
zxl0912 <846833150@qq.com>
2020-12-14 23:52:09 +0800

    update1

---
+++ gitbigdata0820/src/main/java/com/atguigu/linghuchong/dugujiujian.java
@@ -0,0 +1,14 @@
+package com.atguigu.linghuchong;
+
+public class dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
